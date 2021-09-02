4f292d89fa9708aac5b953dac71f99264972ccf6
alisavch <alina.savchuk2001@gmail.com>
2021-09-02 16:55:38 +0300

    Added comment

--- internal/models/history.go
+++ internal/models/history.go
@@ -2,6 +2,7 @@ package models
 
 import "time"
 
+// History is general information about requests.
 type History struct {
 	UploadedName string    `json:"uploaded_name"`
 	ResultedName string    `json:"resulted_name"`
